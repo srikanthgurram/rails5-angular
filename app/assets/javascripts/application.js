ed086c29862ec13e1c8fcd302f5a749581f0e249
sgurram <srikanth.gurram@soctronics.com>
2016-12-13 12:47:29 +0530

    added new bower dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sass-official/assets/javascripts/bootstrap-sprockets
+// = require angular/angular
+//= require angular-ui-router/release/angular-ui-router
 //= require_tree .
